1f8fc511559365702acc2313a9949458718834d9
Rushdi <rushdialhasan@gmail.com>
2019-02-27 21:38:51 -0600

    adding new blog

---
+++ blog/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResumeComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
