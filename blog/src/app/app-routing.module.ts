88ebb3d8a944a3ef12732916c7b69c355d5b9995
Rushdi <rushdialhasan@gmail.com>
2019-02-28 11:39:11 -0600

    added resume and projects component

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProjectsComponent} from './projects/projects.component';
+import { ResumeComponent} from './resume/resume.component';
+import { HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home',component: HomeComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'resume', component: ResumeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ProjectsComponent, ResumeComponent, HomeComponent]
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ResumeComponent } from './resume/resume.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResumeComponent,
-    ProjectsComponent
+    routingComponents,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
